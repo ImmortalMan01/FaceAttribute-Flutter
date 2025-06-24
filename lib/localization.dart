c1a9a024c5d754698da19cbf917fd1c688395ad3
ImmortalMan <ogulcan_topal_2001@hotmail.com>
2025-06-24 20:00:29 +0300

    Replace KBY-AI references with OGULCAN-AI

--- lib/localization.dart
+++ lib/localization.dart
@@ -13,7 +13,7 @@ class AppLocalizations {
     'en': {
       'appTitle': 'Face Recognition',
       'subtitle':
-          'KBY-AI offers SDKs for face recognition, liveness detection, and id document recognition.',
+          'OGULCAN-AI offers SDKs for face recognition, liveness detection, and id document recognition.',
       'enroll': 'Enroll',
       'identify': 'Identify',
       'settings': 'Settings',
@@ -68,14 +68,14 @@ class AppLocalizations {
       'pitch': 'Pitch: ',
       'enterName': 'Enter name',
       'tryAgain': 'Try again',
-      'aboutTitle': 'KBY-AI Technology',
+      'aboutTitle': 'OGULCAN-AI Technology',
       'aboutContent':
           'The Face Recognition System application provides secure and contactless access to buildings using advanced AI and biometric technologies. While it allows only authorized persons to enter, it also simplifies visitor management.\n\nThis project was created by software developer Ogulcan Topal with the dream of making technology a part of daily life. Developed with data privacy and security in mind, it is constantly updated to provide the best user experience.\n\nAbout the developer:\nOgulcan Topal is a developer passionate about artificial intelligence, mobile software, and automation systems. This application reflects his vision of modernizing security with practical solutions.'
     },
     'tr': {
       'appTitle': 'Yüz Tanıma',
       'subtitle':
-          'KBY-AI yüz tanıma, canlılık tespiti ve kimlik belgesi tanıma için SDK\'lar sunar.',
+          'OGULCAN-AI yüz tanıma, canlılık tespiti ve kimlik belgesi tanıma için SDK\'lar sunar.',
       'enroll': 'Kaydet',
       'identify': 'Tanı',
       'settings': 'Ayarlar',
@@ -130,7 +130,7 @@ class AppLocalizations {
       'pitch': 'Eğim: ',
       'enterName': 'İsim girin',
       'tryAgain': 'Tekrar dene',
-      'aboutTitle': 'KBY-AI Teknoloji',
+      'aboutTitle': 'OGULCAN-AI Teknoloji',
       'aboutContent':
           'Yüz Tanıma Sistemi uygulaması, gelişmiş yapay zeka ve biyometrik teknolojilerle desteklenen, binalara güvenli ve temassız erişim sağlamak üzere geliştirilmiştir. Bu uygulama, yalnızca yetkili kişilerin giriş yapmasına olanak tanırken, aynı zamanda ziyaretçi yönetimini de kolaylaştırır.\n\nBu proje, teknolojiyi günlük hayatın bir parçası haline getirme hayaliyle, yazılım geliştiricisi Oğulcan Topal tarafından tasarlanmış ve hayata geçirilmiştir. Uygulama, veri gizliliği ve güvenliği esas alınarak geliştirilmiş olup, kullanıcı deneyimini en üst seviyede tutmak için sürekli olarak güncellenmektedir.\n\nGeliştirici Hakkında:\nOğulcan Topal, yapay zeka, mobil yazılım ve otomasyon sistemlerine tutkuyla bağlı bir geliştiricidir. Bu uygulama, onun pratik çözümlerle güvenliği modernleştirme vizyonunun bir yansımasıdır.'
     }

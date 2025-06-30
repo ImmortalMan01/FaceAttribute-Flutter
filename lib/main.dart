82f3028362d86f396ae8c2d5f19f33aa61a82850
ImmortalMan <ogulcan_topal_2001@hotmail.com>
2025-06-30 07:35:46 +0300

    Arrange home page buttons in grid

--- lib/main.dart
+++ lib/main.dart
@@ -626,26 +626,17 @@ class MyHomePageState extends State<MyHomePage> {
               child: OrientationBuilder(
                 builder: (context, orientation) {
                   final actions = _buildActionButtons();
-                  if (orientation == Orientation.portrait) {
-                    return ListView.separated(
-                      itemCount: actions.length,
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(height: 8),
-                      itemBuilder: (context, index) => actions[index],
-                    );
-                  } else {
-                    return GridView.builder(
-                      itemCount: actions.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        crossAxisSpacing: 12,
-                        mainAxisSpacing: 8,
-                        childAspectRatio: 3.0,
-                      ),
-                      itemBuilder: (context, index) => actions[index],
-                    );
-                  }
+                  return GridView.builder(
+                    itemCount: actions.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount:
+                          orientation == Orientation.portrait ? 2 : 4,
+                      crossAxisSpacing: 12,
+                      mainAxisSpacing: 8,
+                      childAspectRatio: 3.0,
+                    ),
+                    itemBuilder: (context, index) => actions[index],
+                  );
                 },
               ),
             ),

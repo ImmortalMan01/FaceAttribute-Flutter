b70c526fea1fee4e2a4efd403eef65a8c7e74e0e
ImmortalMan <ogulcan_topal_2001@hotmail.com>
2025-06-30 23:35:12 +0300

    Fix missing DartPluginRegistrant import

--- lib/ble_foreground_service.dart
+++ lib/ble_foreground_service.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
